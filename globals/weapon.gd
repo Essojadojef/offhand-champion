2844c06ad4bdc0162f1a24578eb5edd3800c8488
Alessandro Perelli <alessandroperelli30@gmail.com>
2020-06-20 21:09:22 +0200

    Changes to weapons.
    Now weapon script need less code:
    * If root node has a child named "AnimationPlayer", it is used as default value for anim_player.
    * The variable idle_animation_name indicates the animation to play when the weapon returns to its idle state.
    
    Updated weapons scripts and added idle animations.

--- globals/weapon.gd
+++ globals/weapon.gd
@@ -4,6 +4,7 @@ class_name Weapon, "weapon.png"
 var user
 
 var anim_player : AnimationPlayer
+var idle_animation_name : String = "idle"
 var skeleton : Node2D
 
 # properties that can be animated
@@ -15,21 +16,29 @@ signal attack_ended()
 
 
 
+func _ready():
+	if !anim_player and has_node("AnimationPlayer"):
+		anim_player = get_node("AnimationPlayer")
+		anim_player.connect("animation_finished", self, "_on_animation_finished")
+
 func get_attack() -> String: # virtual
 	return ""
 
 func attack(attack_name: String):
-	if anim_player:
-		anim_player.play(attack_name)
-
-func get_current_animation() -> String:
-	return anim_player.current_animation if anim_player else ""
+	anim_player.play(attack_name)
 
 func input_released(): # virtual
 	pass
 
 func cancel():
-	anim_player.seek(anim_player.current_animation.length(), true)
+	end_attack()
+
+func _on_animation_finished(anim_name: String):
+	if anim_name != idle_animation_name:
+		end_attack()
+
+func end_attack():
+	anim_player.play(idle_animation_name)
 	emit_signal("attack_ended")
 
 
@@ -40,4 +49,3 @@ func can_hit(node: Node) -> bool:
 func clash(user_hitbox: Hitbox, opponent_hitbox: Hitbox):
 	emit_signal("clashed", user_hitbox, opponent_hitbox)
 	var opponent_weapon = opponent_hitbox.owner
-
